26df047ef650ffb23721d5c77c39371bb2a7bb74
mjclaypool <mallory.claypool@gmail.com>
2024-09-11 11:33:59 -0400

    Add responsive styling to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 import logo from '../assets/shared/desktop/logo.svg';
 import cart from '../assets/shared/desktop/icon-cart.svg'
 
 export default function Header() {
   return (
-    <header className="flex justify-center bg-n-grey-v-dark p-8">
+    <header className="flex justify-center bg-n-grey-v-dark p-8 md:px-[39px]">
       <div className="flex justify-between items-center w-[1110px] h-7">
-        <div className="flex flex-1 justify-start">
-          <img src={logo} alt="Audiophile logo" className="h-[25px] object-contain" />
+        <div className="xl:hidden">
+          <FontAwesomeIcon
+            icon={faBars}
+            aria-label="Font Awesome Hamburger Menu Icon"
+            className="text-n-white hover:text-p-orange-dark h-[18px] md:pr-[42px] cursor-pointer"
+          />
         </div>
-        <nav className="flex flex-1 justify-center gap-[34px] font-manrope mx-8">
-          <button type="button" className="text-sub-title text-n-white">HOME</button>
-          <button type="button" className="text-sub-title text-n-white">HEADPHONES</button>
-          <button type="button" className="text-sub-title text-n-white">SPEAKERS</button>
-          <button type="button" className="text-sub-title text-n-white">EARPHONES</button>
+        <div className="flex md:flex-1 justify-start">
+          <img src={logo} alt="Audiophile logo" className="h-[25px] object-contain cursor-pointer" />
+        </div>
+        <nav className="hidden xl:flex flex-1 justify-center gap-[34px] font-manrope text-n-white mx-8">
+          <button type="button" className="text-sub-title hover:text-p-orange-dark">HOME</button>
+          <button type="button" className="text-sub-title hover:text-p-orange-dark">HEADPHONES</button>
+          <button type="button" className="text-sub-title hover:text-p-orange-dark">SPEAKERS</button>
+          <button type="button" className="text-sub-title hover:text-p-orange-dark">EARPHONES</button>
         </nav>
-        <div className="flex flex-1 justify-end">
-          <img src={cart} alt="Cart icon" className="h-5 object-contain"/>
+        <div className="flex md:flex-1 justify-end">
+          <img src={cart} alt="Cart icon" className="h-5 object-contain cursor-pointer"/>
         </div>
       </div>
     </header>
