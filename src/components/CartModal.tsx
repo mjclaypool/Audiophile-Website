f4f58521bf5e2d19483cb005af096775fd3bf51d
mjclaypool <mallory.claypool@gmail.com>
2024-09-16 08:47:25 -0400

    Complete responsive Cart Modal

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -20,35 +20,37 @@ export default function CartModal() {
   return createPortal(
     <>
       <dialog
-        className="fixed top-[92px] md:top-[89px] xl:top-[97px] flex justify-center w-full h-full bg-n-grey-v-dark bg-opacity-60 z-40"
+        className="fixed top-[92px] md:top-[89px] xl:top-[97px] flex justify-center w-full h-full bg-n-grey-v-dark bg-opacity-60 z-40 md:px-10"
         open
       >
-        <div className="flex flex-col bg-n-white m-6 px-[28px] py-[32px] md:mt-[57px] w-full h-min font-manrope rounded-lg">
-          <div className="flex justify-between items-center mb-[31px]">
-            <h2 className="text-h6">CART ({cartCtx.cartTotalItems})</h2>
-            <p className="text-body opacity-50 underline cursor-pointer" onClick={handleClearCart}>Remove all</p>
-          </div>
-          {cartCtx.cartItems.length > 0 ?
-            cartCtx.cartItems.map((item) => (
-              <div key={item.id} className="flex justify-between items-center mb-6">
-                <div className="flex items-center">
-                  <img src={`/assets/cart/image-${item.slug}.jpg`} alt={item.name} className="h-16 rounded-lg mr-4" />
-                  <div>
-                    <h3 className="text-body font-bold">{item.name}</h3>
-                    <p className="text-body text-[14px] font-bold opacity-50">$ {priceFormatter.format(item.price)}</p>
+        <div className="flex justify-center md:justify-end w-[1110px]">
+          <div className="flex flex-col bg-n-white m-6 md:mx-0 xl:my-8 px-[28px] py-[32px] w-full md:w-[377px] h-min font-manrope rounded-lg">
+            <div className="flex justify-between items-center mb-[31px]">
+              <h2 className="text-h6">CART ({cartCtx.cartTotalItems})</h2>
+              <p className="text-body opacity-50 underline cursor-pointer" onClick={handleClearCart}>Remove all</p>
+            </div>
+            {cartCtx.cartItems.length > 0 ?
+              cartCtx.cartItems.map((item) => (
+                <div key={item.id} className="flex justify-between items-center mb-6">
+                  <div className="flex items-center">
+                    <img src={`/assets/cart/image-${item.slug}.jpg`} alt={item.name} className="h-16 rounded-lg mr-4" />
+                    <div>
+                      <h3 className="text-body font-bold">{item.name}</h3>
+                      <p className="text-body text-[14px] font-bold opacity-50">$ {priceFormatter.format(item.price)}</p>
+                    </div>
                   </div>
+                  <CartQuantity id={item.id} initial={item.qty} updateQty={handleUpdateQty}/>
                 </div>
-                <CartQuantity id={item.id} initial={item.qty} updateQty={handleUpdateQty}/>
-              </div>
-            ))
-          :
-            <p className="text-body mb-6">Your cart is empty...</p>
-          }
-          <div className="flex justify-between mt-2 mb-6">
-            <h4 className="text-body opacity-50">TOTAL</h4>
-            <p className="text-h6 tracking-[0px]">$ {priceFormatter.format(cartCtx.cartTotalPrice)}</p>
+              ))
+            :
+              <p className="text-body mb-6">Your cart is empty...</p>
+            }
+            <div className="flex justify-between mt-2 mb-6">
+              <h4 className="text-body opacity-50">TOTAL</h4>
+              <p className="text-h6 tracking-[0px]">$ {priceFormatter.format(cartCtx.cartTotalPrice)}</p>
+            </div>
+            <Button color="orange" btnTxt='CHECKOUT'/>
           </div>
-          <Button color="orange" btnTxt='CHECKOUT'/>
         </div>
       </dialog>
     </>,
