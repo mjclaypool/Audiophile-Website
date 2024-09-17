4daa334df19e78a3a853fbec483165ec211184b2
mjclaypool <mallory.claypool@gmail.com>
2024-09-17 09:24:34 -0400

    Update Hero section styling
    Modified gradient overlay and adjusted CTA location.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,16 +6,18 @@ import heroImgMD from "/assets/home/tablet/image-header.jpg";
 import heroImgXL from "/assets/home/desktop/image-hero.jpg";
 
 export default function Hero() {
+  const gradientStyle: string = "h-[508px] md:h-[640px] xl:h-[632px] w-full max-w-[375px] md:max-w-[689px] xl:max-w-[1110px] bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-transparent from-[45%] to-n-grey-v-dark to-[75%] md:to-[80%] xl:to-[90%]";
+
   return (
     <div className="flex justify-center bg-n-grey-v-dark h-[508px] md:h-[640px] xl:h-[632px] mb-[40px] md:mb-[96px] xl:mb-[120px]">
       <div className="absolute z-20 h-[1px] w-[100vw] max-w-[1110px] bg-n-white opacity-10"/>
-      <div className="absolute z-10 top-[0px] h-[600px] md:h-[729px] w-[100vw] bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-transparent from-[30%] to-n-grey-v-dark to-[60%]"/>
+      <div className={`${gradientStyle} absolute z-10 top-[92px] md:top-[89px] xl:top-[97px]`}/>
       <div className="relative flex text-center xl:text-start w-full max-w-[375px] md:max-w-[689px] xl:max-w-[1110px]">
         <div className="relative z-30 flex flex-col font-manrope xl:w-[1110px] text-n-white px-6 ">
           <p className="text-overline opacity-50 mt-[108px] md:mt-[126px] mb-4 md:mb-6">NEW PRODUCT</p>
           <h1 className="text-h2-5 md:text-h1 xl:w-[400px] mb-6">XX99 MARK II HEADPHONES</h1>
           <p className="text-body opacity-75 xl:w-[350px] mb-[28px] md:mb-10 md:px-40 xl:px-0">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-          <Link to="/headphones/xx99-mark-two-headphones" onClick={() => window.scrollTo(0, 0)} className="self-center text-nowrap w-min mb-[112px] md:mb-[167px]">
+          <Link to="/headphones/xx99-mark-two-headphones" onClick={() => window.scrollTo(0, 0)} className="self-center xl:self-start text-nowrap w-min mb-[112px] md:mb-[167px]">
             <Button color="orange" btnTxt="SEE PRODUCT" />
           </Link>
         </div>
